8ff4adcb4a000d646516b6326ba578dcf1c24182
mich101mich <mich101mich@gmail.com>
2020-12-02 08:11:32 +0100

    switched to long number format

--- src/days/day_01.rs
+++ src/days/day_01.rs
@@ -3,7 +3,7 @@ use crate::utils::*;
 #[allow(unused)]
 pub fn run() {
     #[allow(unused_variables)]
-    let input = include_str!("../input/1.txt");
+    let input = include_str!("../input/01.txt");
 
     let parsed = input.lines().map(parse).to_vec();
 
@@ -25,7 +25,7 @@ pub fn run() {
 #[allow(unused)]
 pub fn part_one() {
     #[allow(unused_variables)]
-    let input = include_str!("../input/1.txt");
+    let input = include_str!("../input/01.txt");
 
     let parsed = input.lines().map(parse).to_vec();
 
